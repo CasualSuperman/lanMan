80e1e3d3265df91598247f91c7fe818db0b5f7d8
Bobby Wertman <robert.wertman@gmail.com>
2020-02-29 15:54:05 -0500

    Adjust padding for header item
    Fixes #1

--- src/components/StandardPage.vue
+++ src/components/StandardPage.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="bg-gray-800 pb-32">
       <header-nav v-if="loggedIn" :loggedIn="loggedIn"/>
-      <header class="py-10">
+      <header class="md:pt-4 pb-8">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <h2 class="text-3xl leading-9 font-bold text-white">
             {{$route.name}}
