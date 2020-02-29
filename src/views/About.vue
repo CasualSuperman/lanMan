5657fbbfcaf49bbe6097a38fbae0c7f291876633
James Gaither <jgaither091@gmail.com>
2020-02-29 16:15:48 -0500

    Add a brief description to the about page.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
+<main-card>
+  <div class="h-64 flex items-center justify-center">
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>An app for organizing get-togethers; written by <a class="text-blue-500" href="https://github.com/itege">Itege</a> and <a class="text-blue-500" href="https://github.com/CasualSuperman">CasualSuperman</a></h1>
   </div>
+  </div>
+</main-card>
 </template>
+<script>
+import MainCard from "@/components/MainCard.vue"
+export default {
+  components: {MainCard},
+}
+</script>
